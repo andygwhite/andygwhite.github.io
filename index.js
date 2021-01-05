5ae9f4667f0737a8a403d478c1fef6438af554f3
Andy <andrgwhite@gmail.com>
2021-01-05 12:49:50 -0600

    Created personal landing page

--- index.js
+++
@@ -1,5 +0,0 @@
-const colorButton = document.getElementById("colorbutton");
-const text = document.getElementById("textBox");
-colorButton.addEventListener("click", (e) => {
-    alert("You clicked me!")
-});
