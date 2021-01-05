3f6c6a146de3ab80268eaaecb5e74e01c04fc2be
Andy <andrgwhite@gmail.com>
2021-01-05 12:25:23 -0600

    Added alert to JS file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const colorButton = document.getElementById("colorbutton");
 const text = document.getElementById("textBox");
 colorButton.addEventListener("click", (e) => {
-    
+    alert("You clicked me!")
 });
